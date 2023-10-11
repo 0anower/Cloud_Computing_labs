dda2f2c13d328a8c6dcd4112d79e77629bf5b788
Oboni ANower <144378022+0anower@users.noreply.github.com>
2023-10-11 18:33:16 +0100

    Update collector.py

--- Lab1.3/collector.py
+++ Lab1.3/collector.py
@@ -6,7 +6,11 @@ import requests
 # gives access to command-line arguments and other system-related functionality
 import sys
 
-
+# It uses sys.argv[1] to access the first command-line argument provided when the script is run.
+# sys.argv is a list containing command-line arguments,
+# with sys.argv[0] typically being the script's name and sys.argv[1] being the first argument.
+# It sends an HTTP GET request to the URL specified in the first command-line argument using requests.get().
+# The .json() method is used to parse the JSON response from the URL and store it in the data variable as a Python dictionary.
 server_data = requests.get(sys.argv[1]).json()
 
 count_server = {
